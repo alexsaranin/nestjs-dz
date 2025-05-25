c0dbdc721d07408122957eed7d782753a1d45773
Aleksandr Saranin <alex.saranin.it@gmail.com>
2025-05-25 18:41:56 +0300

    create modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RoomsModule } from './rooms/rooms.module';
+import { BookingScheduleModule } from './booking-schedule/booking-schedule.module';
 
 @Module({
-  imports: [],
+  imports: [RoomsModule, BookingScheduleModule],
   controllers: [AppController],
   providers: [AppService],
 })
